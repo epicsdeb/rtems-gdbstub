1cd53036c5381b7bd7f5baca4d0763ad3155193e
Michael Davidsaver <mdavidsaver@bnl.gov>
2009-07-22 14:41:05 -0400

    Imported Upstream version 1.5

---
+++ html/images.pl
@@ -0,0 +1,35 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/>;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$&gt;$">|; 
+
+$key = q/|;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="9" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\vert$">|; 
+
+$key = q/<;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="17" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$&lt;$">|; 
+
+$key = q/{table}center{tabbing{target:hspace{4em}={bullet{hspace{.5em}=killhost:>{bullet{pace{.5em}>(optional)CEXP{>={version1.5.betatabbing{{{center{{table};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="528" HEIGHT="131" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\begin{table}\begin{center}
+\begin{tabbing}
+target:\hspace{4em}\=$\bullet$\hspac...
+...}\&gt; (optional) CEXP $&gt;=$ version 1.5.beta
+\end{tabbing}
+\end{center}\end{table}">|; 
+
+1;
+
---
+++ html/labels.pl
@@ -0,0 +1,21 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/tbl:prereq/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/tbl:prereq/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+1;
+
